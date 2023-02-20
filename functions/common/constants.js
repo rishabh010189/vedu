f9f4c1ed95a920613372fb100909379806951d95
Rishabh <rishabh010189@gmail.com>
2023-02-21 00:17:34 +0530

    train booking flow complete, car rental started

--- functions/common/constants.js
+++ functions/common/constants.js
@@ -11,7 +11,16 @@ var CONSTANTS = {
       TrainTravelClass : 'Train.travel.class',
       GetLocationPermission : 'Get.location.permission',
       ChangeDestination : 'Change.destination',
-      StartOver: 'Start.over'
+      StartOver: 'Start.over',
+      TrainSeatSelection: 'Train.seat.selection',
+      TrainTravelClass_Yes : 'Train.travel.class - yes',
+      TrainTravelClass_No : 'Train.travel.class - no',
+      MakePayments : 'Payments.make',
+      MakePayments_Completed: 'Payments.make - completed',
+      RentCarIntent: 'Rent.car.intent',
+      RentCarForUser: 'Rent.car.user',
+      RentCarForGuest: 'Rent.car.guest',
+      RentCarForUser_No : 'Rent.car.user - no'
     },
   
     Events : {
@@ -20,12 +29,17 @@ var CONSTANTS = {
       TrainClass : 'event-train-class',
       Welcome : 'event-default-welcome',
       ChangeDestination: 'event-change-destination',
-      StartOver: 'event-start-over'
+      StartOver: 'event-start-over',
+      TrainSeatSelection : 'event-seat-selection',
+      MakePayments: 'event-make-payment',
+      RentCarForUser: 'event-rent-car-user',
+      RentCarForGuest: 'event-rent-car-guest',
     },
   
     Contexts : {
       JourneyStationsData : 'journey-stations-data',
-      ShowBookTicketsData : 'show-book-tickets-data'
+      ShowBookTicketsData : 'show-book-tickets-data',
+      UserTicketsData : 'user-tickets-data'
     }
   }
   
--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,11 @@
 'use strict';
  
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-const {
-  dialogflow,
-  Permission,
-  Suggestions,
-} = require('actions-on-google')
+// const {WebhookClient} = require('dialogflow-fulfillment');
+// const {Card, Suggestion} = require('dialogflow-fulfillment');
+const {dialogflow,Permission,Suggestions, BasicCard, Button, Image} = require('actions-on-google');
+const Nanoid = require('./node_modules/nanoid');
+
 const CONSTANTS = require('./common/constants');
 const utility = require('./common/utility');
 
@@ -19,6 +17,7 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 const app = dialogflow();
 
 let user = {name:'',location:''};
+let isSessionActive = false;
  
   function welcome(agent) {
     conv.ask('🙏 Welcome!!!');
@@ -55,7 +54,7 @@ let user = {name:'',location:''};
         if(user.location && originStation != user.location && user.location !== 'PERMISSION DENIED'){
           conv.ask(new Suggestions(`🚩 ${user.location}`));
         }
-        conv.ask(new Suggestions(`Pune`), new Suggestions(`Start over`));
+        conv.ask(new Suggestions(`Pune`), new Suggestions(`🔁 Start Over`));
       }else{
         conv.ask(`Can you please help me with the destination station?`);
         if(user.location && originStation != user.location && user.location !== 'PERMISSION DENIED'){
@@ -71,7 +70,7 @@ let user = {name:'',location:''};
       conv.followup(CONSTANTS.Events.BookTicket);
     }
     else{
-      const successMsg = `OK! Trying to book a train ticket from ${originStation} to ${destinationStation}...
+      const successMsg = `OK! Trying to book a 🎫train ticket from ${originStation} to ${destinationStation}...
       For when do you want to book the train tickets?`;
       conv.contexts.set(CONSTANTS.Contexts.JourneyStationsData, 3, { '_originStation': originStation, '_destinationStation': destinationStation, 'retryText': successMsg});
       conv.followup(CONSTANTS.Events.TravelPlan);
@@ -128,7 +127,7 @@ let user = {name:'',location:''};
         conv.ask('No trains are available for tomorrow.');
         conv.ask(new Suggestions('Change Date'));
         conv.ask(new Suggestions('Change Destination'));
-        conv.ask(new Suggestions('Start Over'));
+        conv.ask(new Suggestions('🔁 Start Over'));
         conv.parameters.travelDate = null;
         conv.contexts.set(CONSTANTS.Contexts.JourneyStationsData, 3, { '_originStation': originStation, '_destinationStation': destinationStation, 'retryText': 'Please select a new Date.', 'travelDate': null });
       }
@@ -137,12 +136,12 @@ let user = {name:'',location:''};
         conv.ask('date of journey cannot be in past');
         conv.ask(new Suggestions('Change Date'));
         conv.ask(new Suggestions('Change Destination'));
-        conv.ask(new Suggestions('Start Over'));
+        conv.ask(new Suggestions('🔁 Start Over'));
         conv.parameters.travelDate = null;
         conv.contexts.set(CONSTANTS.Contexts.JourneyStationsData, 3, { '_originStation': originStation, '_destinationStation': destinationStation, 'retryText': 'Please select a new Date.', 'travelDate': null });
       }
       else if(utility.isDateValid(travelDate)){
-        conv.contexts.set(CONSTANTS.Contexts.JourneyStationsData, 3, { '_originStation': originStation, '_destinationStation': destinationStation, 'retryText': `Setting travel date for ${travelDate.toLocaleDateString()}`, 'travelDate': travelDate.toLocaleDateString() });
+        conv.contexts.set(CONSTANTS.Contexts.JourneyStationsData, 5, { '_originStation': originStation, '_destinationStation': destinationStation, 'retryText': `Setting travel date for ${travelDate.toLocaleDateString()}`, 'travelDate': travelDate.toLocaleDateString() });
         conv.followup(CONSTANTS.Events.TrainClass);
       }
       
@@ -159,11 +158,11 @@ let user = {name:'',location:''};
     if(!travelClass){
       conv.ask('Which class do you want to travel?')
       conv.ask(new Suggestions('EC'), new Suggestions('1AC'), new Suggestions('2AC'), new Suggestions('3AC'));
-      conv.ask(new Suggestions('Change Destination'), new Suggestions('Start Over'));
+      conv.ask(new Suggestions('Change Destination'), new Suggestions('🔁 Start Over'));
     }
     else{
-      conv.ask('ab to samjho ho gaya');
-      conv.close();
+      conv.ask('Do you want to select seats 💺?');
+      conv.ask(new Suggestions('Yes'), new Suggestions('No'));
     }
   }
 
@@ -176,6 +175,144 @@ let user = {name:'',location:''};
 
   function startOver(conv){
     conv.followup(CONSTANTS.Events.Welcome);
+  }
+
+  function manualSeatSelectionIntent(conv) {
+    conv.followup(CONSTANTS.Events.TrainSeatSelection);
+  }
+
+  function randomSeatSelectionIntent(conv){
+    conv.followup(CONSTANTS.Events.MakePayments);
+  }
+
+  function seatSelection(conv){
+    let context = conv.contexts.get(CONSTANTS.Contexts.JourneyStationsData);
+    let originStation = context.parameters._originStation;
+    let destinationStation = context.parameters._destinationStation;
+    let travelClass = context.parameters.travelClass;
+    let travelDate = context.parameters.travelDate;
+    let seatNumber = conv.parameters.seatNumber;
+    if(!seatNumber){
+      conv.ask(`Awesome! please select your seat/s for your journey from ${originStation} to ${destinationStation} for travel class : ${travelClass} scheduled on ${travelDate}.`);
+      conv.ask(new Suggestions('A1'), new Suggestions('A2'), new Suggestions('B1'), new Suggestions('B2'), new Suggestions('C3'), new Suggestions('C4'));
+    }else{
+      conv.followup(CONSTANTS.Events.MakePayments);
+    }
+  }
+
+  function makePayment(conv){
+    conv.ask('Please complete payment by clicking here');
+    conv.ask(new BasicCard({
+      text: `Please complete payment by clicking ***here***  \n  \n`,
+      subtitle: 'Payment Required',
+      buttons: new Button({
+        title: 'Make Payment',
+        url: 'https://google.com/',
+      }),
+      image: new Image({
+        url: 'https://cdn1.vectorstock.com/i/1000x1000/44/70/woman-make-payment-on-pos-vector-39814470.jpg',
+        alt: 'Please complete payment',
+      }),
+      display: 'CROPPED',
+    }));
+    conv.ask(new Suggestions('Payment Completed'));
+  }
+
+  function paymentReceived(conv){
+    let transactionId = Nanoid.nanoid();
+    let context = conv.contexts.get(CONSTANTS.Contexts.JourneyStationsData);
+    let originStation = context.parameters._originStation;
+    let destinationStation = context.parameters._destinationStation;
+    let travelClass = context.parameters.travelClass;
+    let travelDate = context.parameters.travelDate;
+    // setting a 20 sec timer for active session
+    isSessionActive = true;
+    user.journey = {
+      originStation,
+      destinationStation,
+      travelClass,
+      travelDate
+    }
+    setTimeout(() => {
+      console.log('Session Expired!!!');
+      isSessionActive = false;
+      delete user.journey;
+    }, 20000);
+
+    conv.ask(`Tickets booked with reference ID ${transactionId}`);
+    conv.ask(new BasicCard({
+      text: `Thank You for your payment!  \n Your tickets have been booked and your booking ID is ***${transactionId}***  \n  \n Do you want to rent a car 🚗 also?`,
+      subtitle: 'Payment Received',
+      image: new Image({
+        url: 'https://cashfreelogo.cashfree.com/website/landings/instant-settlements/payment-done.png',
+        alt: 'Payment Received',
+      }),
+      display: 'CROPPED',
+    }));
+    conv.ask(new Suggestions('Yes'), new Suggestions('No'));
+  }
+
+  function rentCarFlowCheck(conv){
+    console.log('Inside rent a car');
+    console.log('Is session Active : ' + isSessionActive);
+    console.log(user.journey);
+    if(isSessionActive && user.journey){
+      // let destinationStation = context.parameters._destinationStation;
+      conv.contexts.set(CONSTANTS.Contexts.UserTicketsData, 3, user.journey);
+      conv.followup(CONSTANTS.Events.RentCarForUser);
+    }else{
+      conv.followup(CONSTANTS.Events.RentCarForGuest);
+    }
+  }
+
+  function rentCarForUser(conv){
+    let context = conv.contexts.get(CONSTANTS.Contexts.UserTicketsData);
+    console.log('Sessioned User');
+    console.log(context);
+    conv.ask('Eurekaaa');
+    if(context){
+      let destinationStation = context.parameters.destinationStation;
+      conv.ask(`Do you want to rent car at your destination city 🏡 ${destinationStation}?`);
+      conv.ask(new Suggestions('Yes'), new Suggestions('No'));
+    }
+  }
+
+  function rentCarForGuest(conv){
+    console.log('Guest');
+    console.log(conv.parameters);
+    let destinationCity = conv.parameters.destinationCity;
+    const todayDate = new Date();
+    const tomorrowDate = new Date((new Date).setDate(todayDate.getDate() + 1));
+    let rentalDate = conv.parameters.rentalDate;
+    rentalDate = new Date(rentalDate);
+    let customDateConstant = 'custom date (dd/mm/yyyy)';
+    if(!destinationCity){
+      conv.parameters.rentalDate = null;
+      conv.ask('Please provide location for your car rental?');
+      conv.ask(new Suggestions('Delhi'), new Suggestions('Jaipur'), new Suggestions('Lucknow'));
+    }
+    else if(conv.query && conv.query === customDateConstant){
+      conv.parameters.rentalDate = null;
+      conv.ask(`Please enter a valid date in the format dd/mm/yyyy`);
+    }
+    else if(!utility.isDateValid(rentalDate)){
+      conv.ask('For when do you want to rent car?');
+      conv.ask(new Suggestions('today'), new Suggestions('tomorrow'), new Suggestions(customDateConstant));
+    }
+    else if(rentalDate.setHours(0,0,0,0) < todayDate.setHours(0,0,0,0)){
+      conv.ask("Rental date cannot be in the past, please select a new date");
+      conv.parameters.rentalDate = null;
+    }
+    else if(utility.isDateValid(rentalDate)){
+      conv.ask('Please proceed to select car type')
+    }
+    else{
+      conv.parameters.rentalDate = null;
+    }
+  }
+
+  function rentCarForUserActionNo(conv){
+    conv.followup(CONSTANTS.Events.RentCarForGuest);
   }
 
   function getLocationPermission(conv, params, granted) {
@@ -199,6 +336,15 @@ let user = {name:'',location:''};
   app.intent(CONSTANTS.Intents.TrainTravelClass, selectTravelClass);
   app.intent(CONSTANTS.Intents.ChangeDestination, changeDestination);
   app.intent(CONSTANTS.Intents.StartOver, startOver);
+  app.intent(CONSTANTS.Intents.TrainTravelClass_Yes, manualSeatSelectionIntent);
+  app.intent(CONSTANTS.Intents.TrainTravelClass_No, randomSeatSelectionIntent);
+  app.intent(CONSTANTS.Intents.TrainSeatSelection, seatSelection)
+  app.intent(CONSTANTS.Intents.MakePayments, makePayment);
+  app.intent(CONSTANTS.Intents.MakePayments_Completed, paymentReceived);
+  app.intent(CONSTANTS.Intents.RentCarIntent, rentCarFlowCheck);
+  app.intent(CONSTANTS.Intents.RentCarForUser, rentCarForUser);
+  app.intent(CONSTANTS.Intents.RentCarForGuest, rentCarForGuest);
+  app.intent(CONSTANTS.Intents.RentCarForUser_No, rentCarForUserActionNo);
 
  // Create a Dialogflow intent with the `actions_intent_PERMISSION` event
   app.intent(CONSTANTS.Intents.GetLocationPermission, getLocationPermission)
